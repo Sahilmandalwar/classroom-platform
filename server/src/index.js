9abfb54576a9b1a207285b746e9ca89f5fdbb79c
sahil Mandalwar <sahilmandalwar30@gmail.com>
2026-06-01 19:47:26 +0530

    cors updated

--- server/src/index.js
+++ server/src/index.js
@@ -27,7 +27,10 @@ const classroomOnlineUsers = new Map();
 
 connectDB()
 
-app.use(cors());
+app.use(cors({
+   origin: process.env.CLIENT_URL,
+   credentials: true
+}));
 app.use(express.json());
 
 app.use('/v1/api/auth',authRoutes);
